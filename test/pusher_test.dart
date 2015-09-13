5a62266a7d12788702218e90bcaf0308805b2c0f
Adão Júnior <itsjunnior@gmail.com>
2015-09-13 12:03:17 -0300

    fix name

--- test/pusher_test.dart
+++ test/pusher_test.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library pg_pubsub.test;
+library pusher.test;
 
 import 'package:pusher/pusher.dart';
 import 'package:test/test.dart';
