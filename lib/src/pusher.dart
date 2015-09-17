2b40934e641c03550885fe20f9ec69d4a126791d
Adão Júnior <itsjunnior@gmail.com>
2015-09-17 11:28:29 -0300

    refactoring docs

--- lib/src/pusher.dart
+++ lib/src/pusher.dart
@@ -35,15 +35,19 @@ class PusherOptions {
   /// Indicates whether calls to the Pusher REST API are over HTTP or HTTPS.
   get encrypted => _encrypted;
 
-  /// Get REST API port that the HTTP calls will be made to.
+  /// port that the HTTP calls will be made to.
   get port => _port;
 }
 
-// todo(): Maybe I should call it Member or Client
+/// Information about a user who is subscribing to a presence channel.
 class User {
 
+  /// A unique user identifier for the user witin the application.
+  /// Pusher uses this to uniquely identify a user. So, if multiple users are given the same [id]
+  /// the second of these users will be ignored and won't be represented on the presence channel.
   final String id;
 
+  /// Arbitrary additional information about the user.
   final Map<String,String> info;
 
   User(this.id,this.info);
