1d5e4cfae4571dab59e9bdf00d86e0b55411dcd7
Adão Junior <itsjunnior@gmail.com>
2015-09-19 13:55:40 -0300

    suport coveralls

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,3 +3,12 @@
 set -o xtrace
 
 pub run test:test 	test/pusher_test.dart
+
+if [ "$COVERALLS_TOKEN" ]; then
+  echo "Running coverage..."
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report --token "$COVERALLS_TOKEN" --debug test/pusher_test.dart
+  echo "Coverage complete..."
+else
+  echo "COVERALLS_TOKEN is unset. Skipping Coverage..."
+fi
