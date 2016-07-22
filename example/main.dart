c95dd84e697550de4fbbf83c4b810d6b3bc3278f
Adao Jr <itsjunnior@gmail.com>
2016-07-22 13:09:54 -0300

    remove unused variable

--- example/main.dart
+++ example/main.dart
@@ -3,5 +3,5 @@ import 'package:pusher/pusher.dart';
 main() async {
   Pusher pusher = new Pusher('PUSHER_APP_ID', 'PUSHER_APP_KEY', 'PUSHER_APP_SECRET');
   Map data = {'message': 'Hello world'};
-  Response result = await pusher.trigger(['test_channel'], 'my_event', data);
+  await pusher.trigger(['test_channel'], 'my_event', data);
 }
