c71e8c4a60ba87059fa4bbf0aaa62999f1a6567f
Richard Muragijimana <beastar457@gmail.com>
2021-04-30 06:34:36 +0200

    add support for null safety

--- lib/src/options.dart
+++ lib/src/options.dart
@@ -5,13 +5,13 @@ const int defaultHttpPort = 80;
 /// Options to be set on the [Pusher] instance.
 class PusherOptions {
   /// Defines a value indicating whether call to the API are over HTTP or HTTPS.
-  bool _encrypted;
+  bool? _encrypted;
 
   int _port = defaultHttpPort;
 
-  String _cluster;
+  String? _cluster;
 
-  PusherOptions({bool encrypted: false, int port, String cluster}) {
+  PusherOptions({bool encrypted: false, int? port, String? cluster}) {
     this._encrypted = encrypted;
 
     if (port != null) this._port = port;
@@ -22,19 +22,19 @@ class PusherOptions {
   }
 
   /// Indicates whether calls to the Pusher REST API are over HTTP or HTTPS.
-  bool get encrypted => _encrypted;
+  bool? get encrypted => _encrypted;
 
   /// port that the HTTP calls will be made to.
   int get port => _port;
 
-  String get cluster => _cluster;
+  String? get cluster => _cluster;
 
   bool get hasCluster => _cluster != null;
 
   String get host => !hasCluster ? defaultHost : "api-${_cluster}.pusher.com";
 
   String getBaseUrl() {
-    String schema = encrypted ? 'https' : 'http';
+    String schema = encrypted! ? 'https' : 'http';
     String port = _port == 80 ? '' : ":${_port}";
     return "$schema://$host$port";
   }
--- lib/src/trigger.dart
+++ lib/src/trigger.dart
@@ -5,32 +5,32 @@ import 'validation.dart';
 /// Options to be set on the trigger method.
 class TriggerOptions {
   /// Socket id to be excluded from receiving event.
-  String _socketId;
+  String? _socketId;
 
-  TriggerOptions({String socketId}) {
+  TriggerOptions({String? socketId}) {
     this._socketId = socketId;
   }
 
   /// Socket id to be excluded from receiving event.
-  String get socketId => _socketId;
+  String? get socketId => _socketId;
 }
 
 /// Represents the payload to be sent when triggering events
 class TriggerBody {
   /// The name of the event
-  final String name;
+  final String? name;
 
   /// The event data
-  final String data;
+  final String? data;
 
   /// The channels the event should be triggered on.
-  final List<String> channels;
+  final List<String>? channels;
 
   /// The id of a socket to be excluded from receiving the event.
-  final String socketId;
+  final String? socketId;
 
   TriggerBody({this.name, this.data, this.channels, this.socketId}) {
-    validateListOfChannelNames(this.channels);
+    validateListOfChannelNames(this.channels!);
     validateSocketId(this.socketId);
   }
 
